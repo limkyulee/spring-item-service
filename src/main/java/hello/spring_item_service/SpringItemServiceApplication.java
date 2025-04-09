1924bd50870dbb7cfadf7841f658b605f2074c3a
kyuleelim <mud0308@gmail.com>
2025-04-09 19:43:59 +0900

    Initial Commit

---
+++ src/main/java/hello/spring_item_service/SpringItemServiceApplication.java
@@ -0,0 +1,13 @@
+package hello.spring_item_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringItemServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringItemServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hello/spring_item_service/SpringItemServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package hello.spring_item_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringItemServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
